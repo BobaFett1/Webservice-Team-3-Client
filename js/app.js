e15d19d4d55440256f7e2682052f9e937b5359df
BobaFett1 <michelehmen@hotmail.de>
2016-06-22 21:08:38 +0200

    Ansatz zum Client

---
+++ js/app.js
@@ -0,0 +1,47 @@
+/**
+ * Created by michelehmen on 22.06.16.
+ */
+var app = angular.module('app', [])
+
+    app.controller('mainController', function($scope, $http) {
+        $scope.formData = {};
+        //$scope.messageData = {};
+        $scope.test ="hallo";
+
+        // Get all messages
+        $http.get('http://webservice-team-3.herokuapp.com/1')
+            .success(function(data) {
+                $scope.messageData = data.userid;
+                console.log(data);
+            })
+            .error(function(error) {
+                $scope.messageData = "nein";
+                console.log('Error: ' + error);
+            });
+        //
+        // // Create a new message
+        // $scope.sendMessage = function() {
+        //     $http.post('/1/message', $scope.formData)
+        //         .success(function(data) {
+        //             $scope.formData = {};
+        //             $scope.messageData = data;
+        //             console.log(data);
+        //         })
+        //         .error(function(error) {
+        //             console.log('Error: ' + error);
+        //         });
+        // };
+        //
+        // // Delete a message
+        // $scope.deleteMessage = function(targetUserID, messageID) {
+        //     $http.delete('/'+targetUserID+'/' + messageID)
+        //         .success(function(data) {
+        //             $scope.messageData = data;
+        //             console.log(data);
+        //         })
+        //         .error(function(data) {
+        //             console.log('Error: ' + data);
+        //         });
+        // };
+
+    });
